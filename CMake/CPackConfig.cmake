fada99c73ac022ad2ac58497fcee2ef2a5e2e315
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2015-08-03 16:01:49 +0200

    Exlude test library from dependent libraries

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -9,4 +9,5 @@ set(CPACK_PACKAGE_CONTACT "Stefan Eilemann <stefan.eilemann@epfl.ch>")
 set(CPACK_PACKAGE_DESCRIPTION_FILE "${PROJECT_SOURCE_DIR}/README.md")
 set(CPACK_PACKAGE_LICENSE "LGPL")
 
+set(ZEROBUF_EXCLUDE_LIBRARIES testschema)
 include(CommonCPack)
